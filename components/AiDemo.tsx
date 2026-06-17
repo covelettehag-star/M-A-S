7d64882790d9334af5041e88d2eedbcbba027b80
covelettehag-star <covelette.hag@gmail.com>
2026-06-17 19:18:14 +0700

    Update AiDemo.tsx

--- components/AiDemo.tsx
+++ components/AiDemo.tsx
@@ -17,17 +17,20 @@ description:
 
 export default function RootLayout({
 children,
-}: Readonly<{
+}: {
 children: React.ReactNode;
-}>) {
+}) {
 return ( <html lang="en" className={poppins.variable}> <body className="font-sans antialiased">
 {children}
 
+```
     <Script
       src="https://buildmyagent.io/widget/6a312c99aaf6a2da575bcbea/widget-professional.js?widgetId=6a312c99aaf6a2da575bcbea"
       strategy="afterInteractive"
     />
   </body>
 </html>
+```
+
 );
 }
