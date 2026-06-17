10560a6904e9e7cf61d65edf2b16032d3095d33c
covelettehag-star <covelette.hag@gmail.com>
2026-06-17 19:34:27 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,6 @@ children: React.ReactNode;
 return ( <html lang="en" className={poppins.variable}> <body className="font-sans antialiased">
 {children}
 
-```
     <Script
       src="https://buildmyagent.io/widget/6a312c99aaf6a2da575bcbea/widget-professional.js?widgetId=6a312c99aaf6a2da575bcbea"></script>
     />
